f7fca281cb401d4d5ad87b7c5e47cc1b245698c3
Nick Prather <nickprather@Nicks-Air.lan>
2024-12-20 16:58:27 -0500

    update: added shadow to navbar to make it more distinct

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <header className="flex flex-row top-0 w-full">
+    <header className="flex flex-row top-0 w-full shadow-md">
       <Link
         href="/"
         className="flex flex-row items-center p-2 m-2 gap-4 bg-slate-800 rounded-3xl md:mr-4 hover:text-slate-300"
